4fcc6969d4869794c44990965e7b4ad425c22c25
Niko <78038534+cooleoooo66@users.noreply.github.com>
2022-02-23 22:18:15 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+window._config = {
+	"apps": [
+		"yt-cl",
+		"comms",
+		"prxyz",
+		"info",
+		"conn_stats",
+		"hacker-news-tile",
+		"sample-games",
+                 "Nikos-games",
+		"good-games"
+	]
+}
